78043a0691338490595fdeddb00b34807d353b1b
asiagodtonegg3beo <87530983+asiagodtonegg3beo@users.noreply.github.com>
2021-08-07 21:59:20 +0800

    BST

---
+++ Binary search Tree/Binary search Tree/Source.cpp
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+#include<stdlib.h>
+using namespace std;
+
+class Node {
+public:
+	Node* lchild;
+	char data;
+	Node* rchild;
+};
+
+Node* createnode(char ch);				/*建構函式*/
+Node* insertnode(Node* root, char ch); /*插入函式*/
+void inorder(Node* root);			   /*中序*/
+void preorder(Node* root);			   /*前序*/
+void postorder(Node* root);			   /*後序*/
+
+
+int main() {
+	Node* root = createnode('A');
+	cout << "存放 " << "A ";
+	char c[] = "BCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (int i = 0; i < strlen(c); i++) {
+		cout << c[i] << " ";
+		insertnode(root, c[i]);
+	}
+	cout << endl;
+	cout << "中序 "; inorder(root); cout << endl;
+	cout << "前序 "; postorder(root); cout << endl;
+	cout << "後序 "; preorder(root); cout << endl;
+}
+
+Node* createnode(char ch) {
+	Node* t = new Node(); /*生成newnode空間*/
+	if (!t) {
+		cout << "Memory error\n";
+		return NULL;
+	}
+	t->data = ch;
+	t->lchild = t->rchild = NULL;
+	return t;
+}
+
+Node* insertnode(Node* root, char ch)
+{
+
+	if (root == NULL) { /*如果樹根是NULL，則create一個node，並將root設為此node*/
+		root = createnode(ch);
+		return root;
+	}
+
+
+	queue<Node*> q; /*queue用來紀錄下一節點有沒有元素，如果有就push，沒有就存放值並結束迴圈*/
+	q.push(root);
+
+	while (!q.empty()) {
+		Node* temp = q.front();
+		q.pop();
+
+		if (temp->lchild != NULL)
+			q.push(temp->lchild);
+		else {
+			temp->lchild = createnode(ch);
+			return root;
+		}
+
+		if (temp->rchild != NULL)
+			q.push(temp->rchild);
+		else {
+			temp->rchild = createnode(ch);
+			return root;
+		}
+	}
+}
+
+void inorder(Node* root) {
+	if (root == NULL) {
+		return;
+	}
+	inorder(root->lchild);
+	cout << root->data << " ";
+	inorder(root->rchild);
+
+}
+
+void preorder(Node* root) {
+	if (root == NULL)
+		return;
+	cout << root->data << " ";
+	preorder(root->lchild);
+	preorder(root->rchild);
+}
+
+void postorder(Node* root) {
+	if (root == NULL)
+		return;
+	postorder(root->lchild);
+	postorder(root->rchild);
+	cout << root->data << " ";
+}
+
+
+
