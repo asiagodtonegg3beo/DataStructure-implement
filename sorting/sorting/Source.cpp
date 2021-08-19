6f16bb55ad1506d76769bf65f92b79bd85b8ad9b
asiagodtonegg3beo <87530983+asiagodtonegg3beo@users.noreply.github.com>
2021-08-19 21:45:06 +0800

    a

--- sorting/sorting/Source.cpp
+++ sorting/sorting/Source.cpp
@@ -8,6 +8,10 @@ int sential(int* data, int key,int n); //效率佳，多一個判斷空間，O(n)
 int *sort(int* data,int n,string ch);
 int BS_it(int* data, int key, int l, int u); //l=lower,u=upper，採疊代方法，O(logn)
 int BS_r(int* data, int key, int l, int u); //採遞迴方法，Binary Search 需先排序
+int IPS(int* data, int key, int l, int u); //插補排序
+int *insertsort(int* data, int n);	//插入排序
+int* selectsort(int* data, int n);
+
 int main() {
 	int data[10] = { 8,5,7,1,4,6,3,2,9,10 };
 	int data_2[10+1];
@@ -20,7 +24,8 @@ int main() {
 	key = 0;
 	//sential(data_2, key,n+1) ? cout << "found " << data_2[sential(data_2, key,n+1)] << " index= " << sential(data_2, key,n+1) : cout << "not found" << endl;
 	string ch = "min";
-	int *p = sort(data,n,ch);
+	int *p =selectsort(data, n);
+	//int *p = sort(data,n,ch);
 	
 	memmove(data, p, sizeof(p));
 
@@ -28,7 +33,9 @@ int main() {
 		printf(" %d ", *(p + i));
 	}
 	cout << endl;
-	BS_r(data, 5, 0, n);
+	//BS_r(data, 5, 0, n);
+	printf("%d",IPS(data, 5, 0, n));
+	
 
 }
 
@@ -115,4 +122,45 @@ int BS_r(int* data, int key, int l, int u) {
 	}
 	printf("not found");
 	return -1;
+}
+
+int IPS(int* data, int key, int l, int u) {
+	if (l <= u)
+	{
+		int i = (key - data[l]) / (data[u] - data[l]) * (u - l);
+		if (data[l + i]==key) return l + i; //if data==key，返回結果
+		else if (data[l + i]>key) return IPS(data, key, l, l + i+1); //找左半邊
+		else return IPS(data, key, l + i+1, u);		//找右半邊
+	}
+	else return -1; //lower>upper，不合理
+}
+
+int *insertsort(int* data, int n) {
+	int j, key;
+	for (int i = 1; i < n; i++) { //i從第2個元素開始比較，j往下遞減
+		key = data[i];
+		j = i;
+		j--;
+		while (key < data[j]) {	
+			data[j + 1] = data[j]; //值往後挪
+			j--;
+		}
+		data[j+1] = key; //key比data[j]大，插在data[j]的後面
+	}
+	return data;
+}
+
+int *selectsort(int* data, int n) {
+	for (int i = 0; i < n-1; i++) { //i從0開始，到n-1結束，當作被比較的對象
+		int min = i;				//min設成i
+		for (int j = i + 1; j < n; j++) {		//j從data[i+1]依序跟data(min(i))做比較
+			if (data[j] < data[min]) {
+				min = j;						//min的index設成j
+			}
+		}
+		if (min != i) {							//每個數字比完，如果min值有變，則交換
+			swap(data[i], data[min]);
+		}
+	}
+	return data;
 }
